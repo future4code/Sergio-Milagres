d081326865c670d0eb27b1a343dfd3a12fe4699a
Sérgio Figorelle <sergiofigorelle@gmail.com>
2021-02-26 13:51:06 -0300

    Finalizei a etapa 'Nosso Jogo' do projeto

--- semana3/blackjack/src/nossoJogo.js
+++ semana3/blackjack/src/nossoJogo.js
@@ -10,3 +10,41 @@
  * 
  * 
  */
+
+console.log("Bem vindo ao jogo de Blackjack!");
+
+for (let i = 0; i === i; i++) {
+  if (confirm("Quer iniciar uma nova rodada?")) {
+    const carta1U = comprarCarta();
+    const carta2U = comprarCarta();
+    const somaU = carta1U.valor + carta2U.valor;
+    console.log(
+      "Usuário - cartas:",
+      carta1U.texto,
+      carta2U.texto,
+      "- pontuação",
+      somaU
+    );
+
+    const carta1PC = comprarCarta();
+    const carta2PC = comprarCarta();
+    const somaPC = carta1PC.valor + carta2PC.valor;
+    console.log(
+      "Computador - cartas:",
+      carta1PC.texto,
+      carta2PC.texto,
+      "- pontuação",
+      somaPC
+    );
+
+    if (somaU > somaPC) {
+      console.log("O usuário ganhou!");
+    } else if (somaU === somaPC) {
+      console.log("Empate!");
+    } else {
+      console.log("O computador ganhou!");
+    }
+  } else {
+    console.log("O jogo acabou");
+  }
+}
