e96a274227c23cdabc92548f740fbab744d54d5a
Sérgio Henrique Figorelle <sergiofigorelle@gmail.com>
2021-03-09 16:51:44 -0300

    Finalizei a Lebedin

--- semana5/labedin/src/App.js
+++ semana5/labedin/src/App.js
@@ -1,51 +1,71 @@
-import React from 'react';
-import './App.css';
-import CardGrande from './components/CardGrande/CardGrande';
-import ImagemButton from './components/ImagemButton/ImagemButton';
+import React from "react";
+import "./App.css";
+import CardGrande from "./components/CardGrande/CardGrande";
+import ImagemButton from "./components/ImagemButton/ImagemButton";
+import CardPequeno from "./components/CardPequeno/CardPequeno";
+
+import perfil from "./img/sergio.jpg";
+import logoCorema from "./img/corema.jpg";
+import logoUniFoa from "./img/unifoa.png";
 
 function App() {
   return (
     <div className="App">
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
-        <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+        <CardGrande
+          imagem={perfil}
+          nome="Sérgio Henrique Nery Figorelle"
+          descricao="Oi, eu sou o Sérgio Henrique. Sou estudante de programação na Labenu. Gosto muito de tecnologia, games, música, cultura pop e outras paradas geeks."
         />
-        
-        <ImagemButton 
-          imagem="https://image.flaticon.com/icons/png/512/117/117472.png" 
+
+        <ImagemButton
+          imagem="https://image.flaticon.com/icons/png/512/117/117472.png"
           texto="Ver mais"
+        />
+
+        <CardPequeno
+          icone="📧"
+          descricao="Email:"
+          info="sergiofigorelle@gmail.com"
+        />
+
+        <CardPequeno
+          icone="🏠"
+          descricao="Endereço:"
+          info="Rua Major Luiz Alves, 317 - Boa Sorte - Barra Mansa, RJ"
         />
       </div>
 
       <div className="page-section-container">
-        <h2>Experiências profissionais</h2>
-        <CardGrande 
-          imagem="https://s3.amazonaws.com/future4.com.br/static/headf4-c492117ca2373dc85ca81bf715b3dc2a.png" 
-          nome="Labenu" 
-          descricao="Formando desenvolvedores para o mercado de trabalho!" 
+        <h2>Experiências profissionais e acadêmicas</h2>
+        <CardGrande
+          imagem={logoCorema}
+          nome="Corema Comércio e Recuperação de Metais"
+          ano="2016 - atualmente"
+          cargo="Auxiliar Administrativo."
+          descricao="Emissão de Notas Fiscais e orçamentos, controle de despesas, relatórios de manutenção preventiva, etc."
         />
-        
-        <CardGrande 
-          imagem="https://imagens.canaltech.com.br/empresas/4418.400.jpg" 
-          nome="NASA" 
-          descricao="Apontando defeitos." 
+
+        <CardGrande
+          imagem={logoUniFoa}
+          nome="Centro Universitário de Volta Redonda - UniFoa"
+          ano="2015 - 2020"
+          descricao="Curso de Engenharia de Produção."
         />
       </div>
 
       <div className="page-section-container">
         <h2>Minhas redes sociais</h2>
-        <ImagemButton 
-          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png" 
-          texto="Facebook" 
-        />        
+        <ImagemButton
+          imagem="https://d2v9ipibika81v.cloudfront.net/uploads/sites/261/2017/01/facebook-logo-3.png"
+          texto="Facebook"
+        />
 
-        <ImagemButton 
-          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png" 
-          texto="Twitter" 
-        />        
+        <ImagemButton
+          imagem="https://logodownload.org/wp-content/uploads/2014/09/twitter-logo-1-1.png"
+          texto="Twitter"
+        />
       </div>
     </div>
   );
