4de46f46e0897b77ce725241cbe9f1f752c78785
Sérgio Figorelle <77934580+sergiohfigorelle@users.noreply.github.com>
2021-05-21 18:15:16 -0300

    Endpoint 3

--- semana15/aula43/src/index.ts
+++ semana15/aula43/src/index.ts
@@ -30,6 +30,35 @@ app.get("/countries/:id", (req: Request, res: Response) => {
   }
 });
 
+// Endpoint 3)
+app.get("/countries/search", (req: Request, res: Response) => {
+  let result: country[] = countries;
+
+  if (req.query.name || req.query.capital || req.query.continent) {
+    if (req.query.name) {
+      result = result.filter((country) =>
+        country.name.includes(req.query.name as string)
+      );
+    }
+
+    if (req.query.capital) {
+      result = result.filter((country) =>
+        country.capital.includes(req.query.capital as string)
+      );
+    }
+
+    if (req.query.continent) {
+      result = result.filter((country) =>
+        country.continent.includes(req.query.continent as string)
+      );
+    }
+
+    res.status(200).send(result);
+  } else {
+    res.status(400).send("Invalid Parameters");
+  }
+});
+
 app.listen(3003, () => {
   console.log("Servidor rodando na porta 3003");
 });
