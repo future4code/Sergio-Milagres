aca78dca7461043740d922305192aa47b24f2abe
Sérgio Figorelle <77934580+sergiohfigorelle@users.noreply.github.com>
2021-05-31 21:29:36 -0300

    Exercício 2

--- semana17/aula49/src/index.ts
+++ semana17/aula49/src/index.ts
@@ -3,6 +3,7 @@ import knex from "knex";
 import cors from "cors";
 import dotenv from "dotenv";
 import { AddressInfo } from "net";
+import { searchUserInput } from "./types/user";
 
 dotenv.config();
 
@@ -24,48 +25,68 @@ app.use(cors());
 // Exercicio 1
 
 // a)
+/* app.get("/user/search", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const { name } = req.query;
+    if (!name) {
+      res.statusCode = 422;
+      throw new Error(`"nome" é obrigatório!`);
+    }
+    const users: any = await connection
+      .select("*")
+      .from("aula49_exercicio")
+      .where("name", "LIKE", `%${name}%`);
+
+    if (!users.length) {
+      res.statusCode = 404;
+      throw new Error("Nenhum usuário encontrado");
+    }
+    res.send(users);
+  } catch (error) {
+    res.send(error.sqlMessage || error.message);
+  }
+});*/
+
+// b)
+app.get("/user/:type", async (req: Request, res: Response): Promise<void> => {
+  try {
+    const type: string = req.params.type;
+
+    const users: any = await connection
+      .select("*")
+      .from("aula49_exercicio")
+      .where("type", "LIKE", `%${type}%`);
+
+    if (!users.length) {
+      res.statusCode = 404;
+      throw new Error("Nenhum usuário encontrado");
+    }
+
+    res.send(users);
+  } catch (error) {
+    res.send(error.sqlMessage || error.message);
+  }
+});
+
+// Exercicio 2
 app.get("/user/search", async (req: Request, res: Response): Promise<void> => {
-    try {
-      const { name } = req.query;
-      if (!name) {
-        res.statusCode = 422;
-        throw new Error(`"nome" é obrigatório!`);
-      }
-      const users: any = await connection
-        .select("*")
-        .from("aula49_exercicio")
-        .where("name", "LIKE", `%${name}%`);
-  
-      if (!users.length) {
-        res.statusCode = 404;
-        throw new Error("Nenhum usuário encontrado");
-      }
-      res.send(users);
-    } catch (error) {
-      res.send(error.sqlMessage || error.message);
+  try {
+    const { orderBy = "name", orderType = "ASC" } =
+      req.query as searchUserInput;
+    const users: any = await connection
+      .select("*")
+      .from("aula49_exercicio")
+      .orderBy(orderBy, orderType);
+
+    if (!users.length) {
+      res.statusCode = 404;
+      throw new Error("Nenhum usuário encontrado");
     }
-  });
-  
-  // b)
-  app.get("/user/:type", async (req: Request, res: Response): Promise<void> => {
-    try {
-      const type: string = req.params.type;
-  
-      const users: any = await connection
-        .select("*")
-        .from("aula49_exercicio")
-        .where("type", "LIKE", `%${type}%`);
-  
-      if (!users.length) {
-        res.statusCode = 404;
-        throw new Error("Nenhum usuário encontrado");
-      }
-  
-      res.send(users);
-    } catch (error) {
-      res.send(error.sqlMessage || error.message);
-    }
-  });
+    res.send(users);
+  } catch (error) {
+    res.send(error.sqlMessage || error.message);
+  }
+});
 
 // Servidor
 const server = app.listen(process.env.PORT || 3003, () => {
